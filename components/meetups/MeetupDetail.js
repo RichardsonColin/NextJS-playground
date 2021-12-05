ba2726e798716c81b2eed6cb0105b347e6a2ed18
Colin Richardson <colin.richardson.dev@gmail.com>
2021-12-04 19:23:35 -0600

    add next Image Component with standard props

--- components/meetups/MeetupDetail.js
+++ components/meetups/MeetupDetail.js
@@ -1,9 +1,16 @@
+import Image from 'next/image';
 import classes from './MeetupDetail.module.css';
 
 function MeetupDetail(props) {
   return (
     <section className={classes.detail}>
-      <img src={props.image} alt={props.title} />
+      <Image
+        src={props.image}
+        alt={props.title}
+        width={600}
+        height={400}
+        layout='responsive'
+      />
       <h1>{props.title}</h1>
       <address>{props.address}</address>
       <p>{props.description}</p>
