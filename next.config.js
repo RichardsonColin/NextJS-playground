1b8fd10d8d7d0c1e6b3f43f99020c3c4212db797
Colin Richardson <colin.richardson.dev@gmail.com>
2021-12-07 18:40:48 -0600

    include image asset source host to configure image optimization

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
 const DB_URI = `mongodb+srv://${process.env['DB_USER']}:${process.env['DB_PASSWORD']}${process.env['DB_HOST']}/${process.env['DB_NAME']}?${process.env['DB_OPTIONS']}`;
 const DB_NAME = process.env['DB_NAME'];
 const fallback = process.env.NODE_ENV === 'development' ? false : 'blocking';
+const domains = ['i.imgur.com'];
 
 module.exports = (phase) => {
   if (phase === PHASE_DEVELOPMENT_SERVER) {
@@ -12,6 +13,9 @@ module.exports = (phase) => {
         DB_NAME,
         fallback,
       },
+      images: {
+        domains,
+      },
     };
   }
 
@@ -20,6 +24,9 @@ module.exports = (phase) => {
       DB_URI,
       DB_NAME,
       fallback,
+    },
+    images: {
+      domains,
     },
   };
 };
