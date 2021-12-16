6ce683031c82b9cc15ee54bc0669f275253064e8
Colin Richardson <colin.richardson.dev@gmail.com>
2021-12-15 18:17:38 -0600

    adds helper methods to convert image by url to data url

--- models/meetup.js
+++ models/meetup.js
@@ -1,5 +1,9 @@
 import connectToDatabase from '../lib/database/index';
 
+// libs
+import imgur from '../lib/imgur/api';
+import sharp from '../lib/sharp/api';
+
 /*
   TODO:
   - add try/catch
@@ -25,10 +29,79 @@ const fetchAll = async (query = {}, options = {}) => {
 };
 
 const createOne = async (data) => {
-  const { db } = await connectToDatabase();
-  const collection = db.collection(COLLECTION);
-  const result = await collection.insertOne(data);
-  return result;
+  try {
+    const { db } = await connectToDatabase();
+    const collection = db.collection(COLLECTION);
+    const preparedData = await prepareDataForCreate(data);
+    const result = await collection.insertOne(preparedData);
+    return result;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// model helpers
+
+const prepareDataForCreate = async (initialData) => {
+  const data = { ...initialData };
+
+  if (data && 'image' in data) {
+    // upload to imgur for imgur link and update data
+    data.image = (await imgur.upload(data.image)).link;
+    // generate a data url from the image
+    data.dataUrl = await generateImageDataUrl(data.image, (buffer) =>
+      sharp.resizeImage(buffer, 100, 100)
+    );
+  }
+
+  return data;
+};
+
+const fetchImage = async (imageUrl) => {
+  try {
+    const response = await fetch(imageUrl);
+
+    if (!response.ok) {
+      throw new Error(response);
+    }
+
+    return response;
+  } catch (error) {
+    console.error(`Meetup fetchImage: ${error}`);
+    return false;
+  }
+};
+
+// generates a minimal base64 data url from an image
+const generateImageDataUrl = async (imageUrl, resize) => {
+  const acceptedMIMETypes = [
+    'apng',
+    'avif',
+    'gif',
+    'jpeg',
+    'png',
+    'svg+xml',
+    'webp',
+  ];
+  try {
+    const response = await fetchImage(imageUrl);
+    const contentType = response.headers.get('content-type');
+
+    if (contentType && acceptedMIMETypes.includes(contentType.split('/')[1])) {
+      let buffer = await response.buffer();
+
+      if (resize && typeof resize === 'function') {
+        buffer = await resize(buffer);
+      }
+
+      return `data:${contentType};base64,${buffer.toString('base64')}`;
+    } else {
+      throw new Error(`Can't generate a data URL from an improper file type.`);
+    }
+  } catch (error) {
+    console.error(`Meetup buildImageDataUrl: ${error}`);
+    return false;
+  }
 };
 
 export default {
