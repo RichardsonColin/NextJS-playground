3abaf7ec0fdf964d38ac2ca578ebf6da3fd9ac8d
Colin Richardson <colin.richardson.dev@gmail.com>
2021-12-15 19:17:51 -0600

    comment out model code to check deploy error

--- models/meetup.js
+++ models/meetup.js
@@ -32,9 +32,9 @@ const createOne = async (data) => {
   try {
     const { db } = await connectToDatabase();
     const collection = db.collection(COLLECTION);
-    // const preparedData = await prepareDataForCreate(data);
-    // const result = await collection.insertOne(preparedData);
-    // return result;
+    /*onst preparedData = await prepareDataForCreate(data);
+    const result = await collection.insertOne(preparedData);
+    return result;*/
     return '';
   } catch (error) {
     console.error(error);
@@ -43,67 +43,68 @@ const createOne = async (data) => {
 
 // model helpers
 
-// const prepareDataForCreate = async (initialData) => {
-//   const data = { ...initialData };
+/*const prepareDataForCreate = async (initialData) => {
+  const data = { ...initialData };
 
-//   if (data && 'image' in data) {
-//     // upload to imgur for imgur link and update data
-//     data.image = (await imgur.upload(data.image)).link;
-//     // generate a data url from the image
-//     data.dataUrl = await generateImageDataUrl(data.image, (buffer) =>
-//       sharp.resizeImage(buffer, 100, 100)
-//     );
-//   }
+  if (data && 'image' in data) {
+    // upload to imgur for imgur link and update data
+    data.image = (await imgur.upload(data.image)).link;
+    // generate a data url from the image
+    data.dataUrl = await generateImageDataUrl(data.image, (buffer) =>
+      sharp.resizeImage(buffer, 100, 100)
+    );
+  }
 
-//   return data;
-// };
+  return data;
+};
 
-// const fetchImage = async (imageUrl) => {
-//   try {
-//     const response = await fetch(imageUrl);
+const fetchImage = async (imageUrl) => {
+  try {
+    const response = await fetch(imageUrl);
 
-//     if (!response.ok) {
-//       throw new Error(response);
-//     }
+    if (!response.ok) {
+      throw new Error(response);
+    }
 
-//     return response;
-//   } catch (error) {
-//     console.error(`Meetup fetchImage: ${error}`);
-//     return false;
-//   }
-// };
+    return response;
+  } catch (error) {
+    console.error(`Meetup fetchImage: ${error}`);
+    return false;
+  }
+};
 
-// // generates a minimal base64 data url from an image
-// const generateImageDataUrl = async (imageUrl, resize) => {
-//   const acceptedMIMETypes = [
-//     'apng',
-//     'avif',
-//     'gif',
-//     'jpeg',
-//     'png',
-//     'svg+xml',
-//     'webp',
-//   ];
-//   try {
-//     const response = await fetchImage(imageUrl);
-//     const contentType = response.headers.get('content-type');
+// generates a minimal base64 data url from an image
+const generateImageDataUrl = async (imageUrl, resize) => {
+  const acceptedMIMETypes = [
+    'apng',
+    'avif',
+    'gif',
+    'jpeg',
+    'png',
+    'svg+xml',
+    'webp',
+  ];
+  try {
+    const response = await fetchImage(imageUrl);
+    const contentType = response.headers.get('content-type');
 
-//     if (contentType && acceptedMIMETypes.includes(contentType.split('/')[1])) {
-//       let buffer = await response.buffer();
+    if (contentType && acceptedMIMETypes.includes(contentType.split('/')[1])) {
+      let buffer = await response.buffer();
 
-//       if (resize && typeof resize === 'function') {
-//         buffer = await resize(buffer);
-//       }
+      if (resize && typeof resize === 'function') {
+        buffer = await resize(buffer);
+      }
 
-//       return `data:${contentType};base64,${buffer.toString('base64')}`;
-//     } else {
-//       throw new Error(`Can't generate a data URL from an improper file type.`);
-//     }
-//   } catch (error) {
-//     console.error(`Meetup buildImageDataUrl: ${error}`);
-//     return false;
-//   }
-// };
+      return `data:${contentType};base64,${buffer.toString('base64')}`;
+    } else {
+      throw new Error(`Can't generate a data URL from an improper file type.`);
+    }
+  } catch (error) {
+    console.error(`Meetup buildImageDataUrl: ${error}`);
+    return false;
+  }
+};
+*/
 
 export default {
   fetchOne,
