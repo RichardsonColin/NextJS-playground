9ee5b77f4e33a48bcdea54c87f51d5b7acc2bdb7
Colin Richardson <colin.richardson.dev@gmail.com>
2021-12-16 07:45:17 -0600

    removes code to try and fix netlify deploy

--- models/meetup.js
+++ models/meetup.js
@@ -2,7 +2,6 @@ import connectToDatabase from '../lib/database/index';
 
 // libs
 import imgur from '../lib/imgur/api';
-// import sharp from '../lib/sharp/api';
 
 /*
   TODO:
@@ -32,7 +31,7 @@ const createOne = async (data) => {
   try {
     const { db } = await connectToDatabase();
     const collection = db.collection(COLLECTION);
-    /*onst preparedData = await prepareDataForCreate(data);
+    /*const preparedData = await prepareDataForCreate(data);
     const result = await collection.insertOne(preparedData);
     return result;*/
     return '';
@@ -40,71 +39,6 @@ const createOne = async (data) => {
     console.error(error);
   }
 };
-
-// model helpers
-
-/*const prepareDataForCreate = async (initialData) => {
-  const data = { ...initialData };
-
-  if (data && 'image' in data) {
-    // upload to imgur for imgur link and update data
-    data.image = (await imgur.upload(data.image)).link;
-    // generate a data url from the image
-    data.dataUrl = await generateImageDataUrl(data.image, (buffer) =>
-      sharp.resizeImage(buffer, 100, 100)
-    );
-  }
-
-  return data;
-};
-
-const fetchImage = async (imageUrl) => {
-  try {
-    const response = await fetch(imageUrl);
-
-    if (!response.ok) {
-      throw new Error(response);
-    }
-
-    return response;
-  } catch (error) {
-    console.error(`Meetup fetchImage: ${error}`);
-    return false;
-  }
-};
-
-// generates a minimal base64 data url from an image
-const generateImageDataUrl = async (imageUrl, resize) => {
-  const acceptedMIMETypes = [
-    'apng',
-    'avif',
-    'gif',
-    'jpeg',
-    'png',
-    'svg+xml',
-    'webp',
-  ];
-  try {
-    const response = await fetchImage(imageUrl);
-    const contentType = response.headers.get('content-type');
-
-    if (contentType && acceptedMIMETypes.includes(contentType.split('/')[1])) {
-      let buffer = await response.buffer();
-
-      if (resize && typeof resize === 'function') {
-        buffer = await resize(buffer);
-      }
-
-      return `data:${contentType};base64,${buffer.toString('base64')}`;
-    } else {
-      throw new Error(`Can't generate a data URL from an improper file type.`);
-    }
-  } catch (error) {
-    console.error(`Meetup buildImageDataUrl: ${error}`);
-    return false;
-  }
-};
-*/
 
 export default {
   fetchOne,
