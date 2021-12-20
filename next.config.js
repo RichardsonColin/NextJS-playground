7346c84db8b486e9a2f71107bd9e7a7ce4ec1024
Colin Richardson <colin.richardson.dev@gmail.com>
2021-12-20 16:06:19 -0600

    refactor to single environment

--- next.config.js
+++ next.config.js
@@ -1,31 +1,26 @@
-const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
-
-const DB_URI = `mongodb+srv://${process.env['DB_USER']}:${process.env['DB_PASSWORD']}${process.env['DB_HOST']}/${process.env['DB_NAME']}?${process.env['DB_OPTIONS']}`;
-const DB_NAME = process.env['DB_NAME'];
+const DB_URI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}${process.env.DB_HOST}/${process.env.DB_NAME}?${process.env.DB_OPTIONS}`;
+const DB_NAME = process.env.DB_NAME;
+const HOST_URL =
+  process.env.NODE_ENV === 'production'
+    ? `https://${process.env.SITE_NAME}.netlify.app`
+    : process.env.NETLIFY_DEV
+    ? `${process.env.DEV_URL}:8888`
+    : `${process.env.DEV_URL}:3000`;
+const FUNCTIONS_DIR = process.env.SITE_NAME
+  ? '/.netlify/functions'
+  : '/functions';
 const fallback = process.env.NODE_ENV === 'development' ? false : 'blocking';
 const imgDomains = ['i.imgur.com'];
 const imgFormats = ['image/avif', 'image/webp'];
 
-module.exports = (phase) => {
-  if (phase === PHASE_DEVELOPMENT_SERVER) {
-    return {
-      env: {
-        DB_URI,
-        DB_NAME,
-        fallback,
-      },
-      images: {
-        domains: imgDomains,
-        formats: imgFormats,
-      },
-    };
-  }
-
+module.exports = () => {
   return {
     env: {
       DB_URI,
       DB_NAME,
       fallback,
+      HOST_URL,
+      FUNCTIONS_DIR,
     },
     images: {
       domains: imgDomains,
