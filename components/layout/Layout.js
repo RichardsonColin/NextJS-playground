58e57f68b53c5baa6f7eef55a0ab1bdb74bd54c3
Colin Richardson <colin.richardson.dev@gmail.com>
2021-12-21 18:19:38 -0600

    converts styling into styled-components

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,13 +1,21 @@
 import PropTypes from 'prop-types';
+
 import MainNavigation from './MainNavigation';
-import classes from './Layout.module.css';
+
+import styled from 'styled-components';
+
+const Main = styled.main`
+  margin: 3rem auto;
+  width: 90%;
+  max-width: 40rem;
+`;
 
 function Layout(props) {
   return (
-    <div>
+    <>
       <MainNavigation />
-      <main className={classes.main}>{props.children}</main>
-    </div>
+      <Main>{props.children}</Main>
+    </>
   );
 }
 
