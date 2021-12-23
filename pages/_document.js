6263a8619799ceae1ee8b1139c7cd962d585a74f
Colin Richardson <colin.richardson.dev@gmail.com>
2021-12-23 10:16:18 -0600

    fixes SSR of styled-components

--- pages/_document.js
+++ pages/_document.js
@@ -1,28 +1,42 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
-// import styled components ServerStyleSheet
 import { ServerStyleSheet } from 'styled-components';
 
+/*
+  Custom _document.js to support proper SSR of styled-components
+    - docs: https://styled-components.com/docs/advanced#server-side-rendering
+    - example: https://github.com/vercel/next.js/blob/master/examples/with-styled-components/pages/_document.js
+*/
 export default class MyDocument extends Document {
-  static getInitialProps({ renderPage }) {
-    // creates an instance of ServerStyleSheet
-    const sheet = new ServerStyleSheet();
+  static async getInitialProps(context) {
+    const styleSheet = new ServerStyleSheet();
+    const originalRenderPage = context.renderPage;
 
-    // retrieves styles from components in the page
-    const page = renderPage(
-      (App) => (props) => sheet.collectStyles(<App {...props} />)
-    );
+    try {
+      context.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            styleSheet.collectStyles(<App {...props} />),
+        });
 
-    // extracts the styles as <style> tags
-    const styleTags = sheet.getStyleElement();
-
-    // passes styleTags as a prop
-    return { ...page, styleTags };
+      const initialProps = await Document.getInitialProps(context);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {styleSheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      styleSheet.seal();
+    }
   }
 
   render() {
     return (
       <Html>
-        <Head>{this.props.styleTags}</Head>
+        <Head>{this.props.styles}</Head>
         <body>
           <Main />
           <div id='toast' />
